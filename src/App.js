b2db6535b9b25e738095ea73ee7843170e697712
vrunafederico <Federix5@hotmail.com>
2023-03-24 17:18:10 +0100

    Props-1

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Walcome } from "./Welcome";
+
+export class App extends React.Component{
+    render(){
+        return(
+            <Walcome name="francesco" />
+        )
+    }
+}
+
---
+++ src/Welcome.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export class Walcome extends React.Component{
+    render(){
+        return (
+            <h1>
+                Welcome {this.props.name}
+            </h1>
+        )
+    }
+}
