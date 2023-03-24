70f2c8a4121ce8cda68e3ad58e161235b7a9a65e
vrunafederico <Federix5@hotmail.com>
2023-03-24 19:48:21 +0100

    Conditional-Rendering-3

--- src/Welcome.js
+++ src/Welcome.js
@@ -5,7 +5,9 @@ export class Walcome extends React.Component {
     return (
       <>
         <h1>Welcome {this.props.name}</h1>
-        {this.props.age && <Age age={this.props.age}/>}
+        {this.props.age > 18 && this.props.age < 65 && (
+          <Age age={this.props.age} />
+        )}
       </>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import ReactDOM from "react-dom";
 ReactDOM.render(<App />, document.getElementById("app"));
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Name name={<strong>giulio</strong>} />);
+root.render(<Name name={<strong>giulio</strong>} age={19} />);
 
 export function Name(props) {
   return (
     <>
       <h1>Walcome {props.name ?? "francesco"}</h1>
-      {props.age && <Age2 age={props.age}/>}
+      {props.age > 18 && props.age < 65 && <Age2 age={props.age} />}
     </>
   );
 }
