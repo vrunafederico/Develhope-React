a5009d048974a8901b84eda68078bde4c3093822
vrunafederico <122743597+vrunafederico@users.noreply.github.com>
2023-03-25 15:03:31 +0100

    Conditional-Rendering-5

--- src/Welcome.js
+++ src/Welcome.js
@@ -15,7 +15,7 @@ export class Walcome extends React.Component {
 
 export class Age extends React.Component {
   render() {
-    return (this.props.age > 18 ? <p>Your age is {this.props.age}</p> : <p>Sei molto giovane!</p>)
+    return (this.props.age > 18 ? <p>Your age is {this.props.age}</p> : <p>You are very young!</p>)
   }
 }
 
