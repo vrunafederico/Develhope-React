dee754751e2d6f93c85a24df7effd3f83a44cf89
Federicovruna <122454290+Federicovruna@users.noreply.github.com>
2023-03-26 23:28:19 +0200

    State-1

--- src/index.js
+++ src/index.js
@@ -1,15 +1,43 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Counter } from "./Counter";
 import  ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom'
 
 ReactDOM.render(<Counter />, document.getElementById("Root"));
-const Root = createRoot(document.getElementById("Root"));
+const Root = createRoot(document.getElementById("App"));
 
-Root.render(){
-    const [count, setCount] = useState(0);
+Root.render(
+    <>
+        <Counter2 />
+        <Counter3 />
+    </> 
+    
+)
+
+
+export function Counter2(){
+    let [count, setCount] = useState(0);
 
     setInterval(()=>{
-        setCount +1;
+        setCount(() => {
+            return count = count + 1
+        })
+    },1000)
+
+    return (
+        <h1>{count}</h1>
+    )
+}
+
+export function Counter3(){
+    const [count, setCount] = useState(0);
+
+    useEffect(()=>{
+        setTimeout(()=>{
+            setCount(() => {
+                return count + 1
+            })
+        },1000)
     })
 
     return (
