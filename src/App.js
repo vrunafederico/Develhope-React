70a542c7dd9da0fbe9588a7f049d5fa6572f6893
vrunafederico <Federix5@hotmail.com>
2023-04-07 23:31:52 +0200

    Context-1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from "react";
-import { Walcome } from "./Welcome";
+import { LanguageContext } from "./LanguageContext";
 
-export class App extends React.Component{
-    render(){
-        return(
-            <Walcome name="francesco" />
-        )
+export class App extends React.Component {
+    state = {
+        language: "en",
+    };
+
+    handleLanguageChange = (e) =>{
+        this.setState({
+            language: e.target.value
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <select value={this.state.language} onChange={this.handleLanguageChange}>
+                    <option value="en">inglese</option>
+                    <option value="it">italiano</option>
+                    <option value="es">spagnolo</option>
+                    <option value="da">tedesco</option>
+                </select>
+                <LanguageContext.Provider value={this.state.language}>
+       
+                </LanguageContext.Provider>
+            </div>
+        );
     }
 }
-
--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 import { App } from "./App";
 import ReactDOM  from "react-dom";
-import { render } from "@testing-library/react";
 
 ReactDOM.render(<App />, document.getElementById("app"));
-const root = ReactDOM.createRoot(document.getElementById("root"));
 
 
-root.render(
-    <Name name="Giovanni"/>
-)
-
-
-export function Name(props){
-    return(
-        <h1>Walcome {props.name}</h1>
-    )
-}
