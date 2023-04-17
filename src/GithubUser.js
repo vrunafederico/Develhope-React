95ce319e1da885fcf18fbbe5b526605c66f93c2d
vrunafederico <Federix5@hotmail.com>
2023-04-17 10:14:28 +0200

    Custom-Hooks-4

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useGithubUser } from "./useGithubUser";
 
 export function GithubUser({username}){
-    const {data} = useGithubUser(username)
+    const {onSearch, data, error, loading} = useGithubUser(username)
 
+    useEffect(()=>{
+        onSearch(username)
+    },[])
+        
     return(
-        data && <div style={{display:"flex", flexDirection:"column"}}>
-            <span>{data.avatar_url}</span>
-            <span>{data.id}</span>
-            <span>{data.login}</span>
-            <span>{data.type}</span>
-        </div>
+        <>
+            {error && <div>{error}</div>}
+            {loading && <div>loading</div>}
+            {data && <div style={{display:"flex", flexDirection:"column"}}>
+                <span>{data.avatar_url}</span>
+                <span>{data.id}</span>
+                <span>{data.login}</span>
+                <span>{data.type}</span>
+            </div>}
+        </>
     )
 }
--- src/useGithubUser.js
+++ src/useGithubUser.js
@@ -1,18 +1,27 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 
-export function useGithubUser(username){
+export function useGithubUser(username) {
     const [data, setData] = useState();
+    const [error, setError] = useState();
+    const [loading, setLoading] = useState(true);
 
-    useEffect(()=>{
-        fetch(`https://api.github.com/users/${username}`)
-            .then(responce => responce.json())
-            .then(result => setData(result))
-            .catch(error => console.log(error))
-    },[username])
-
-    console.log(data)
-
-    return{
-        data: data
+    async function search(){
+        setLoading(true)
+        try{
+            const responce = await  fetch(`https://api.github.com/users/${username}`)
+            const json = await responce.json()
+            setData(json)
+        }catch(error){
+            setError(error)
+        }finally{
+            setLoading(false)
+        }
     }
+
+    return {
+        onSearch: search,
+        data: data,
+        error: error,
+        loading: loading,
+    };
 }
