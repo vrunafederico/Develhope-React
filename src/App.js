671405d7516fe08884c006620374a4762029a356
vrunafederico <Federix5@hotmail.com>
2023-04-22 11:12:43 +0200

    UseContext

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { LanguageContext } from "./LanguageContext";
 import { Language } from "./Language";
 
-export class App extends React.Component {
-    state = {
-        language: "en",
-    };
+export function App() {
+  const [language, setLanguage] = useState("en");
 
-    handleLanguageChange = (e) => {
-        this.setState({
-            language: e.target.value,
-        });
-    };
+  const handleLanguageChange = (e) => {
+    setLanguage(e.target.value);
+  };
 
-    render() {
-        return (
-            <div>
-                <select value={this.state.language} onChange={this.handleLanguageChange}>
-                    <option value="en">inglese</option>
-                    <option value="it">italiano</option>
-                    <option value="es">spagnolo</option>
-                    <option value="da">tedesco</option>
-                </select>
-                <LanguageContext.Provider value={this.state.language}>
-                    <Language />
-                </LanguageContext.Provider>
-            </div>
-        );
-    }
+  return (
+    <div>
+      <select value={language} onChange={handleLanguageChange}>
+        <option value="en">inglese</option>
+        <option value="it">italiano</option>
+        <option value="es">spagnolo</option>
+        <option value="da">tedesco</option>
+      </select>
+      <LanguageContext.Provider value={language}>
+        <Language />
+      </LanguageContext.Provider>
+    </div>
+  );
 }
--- src/Language.js
+++ src/Language.js
@@ -1,14 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { LanguageContext } from "./LanguageContext";
 
-export class Language extends React.Component {
-    render() {
-        return (
-            <LanguageContext.Consumer>
-                {(language) => {
-                    return <div>{language}</div>;
-                }}
-            </LanguageContext.Consumer>
-        );
-    }
+export function Language() {
+  const language = useContext(LanguageContext);
+
+  return (
+    <div>{language}</div>
+  );
 }
